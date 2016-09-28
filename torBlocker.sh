2c5092d3f9b18837fc6155badf33461fcd7ec114
Luke <bob_monday@outlook.com>
2016-09-28 12:58:55 +0100

    Create torBlocker.sh

---
+++ torBlocker.sh
@@ -0,0 +1,27 @@
+# # # # # # # # # # # #         
+# Author: Unknown     #
+#                     #
+# Modified by LukeBob #
+# # # # # # # # # # # #
+
+#!/bin/bash
+
+#Block tor exit nodes
+IPTABLES_TARGET="DROP"
+IPTABLES_CHAINNAME="TOR"
+if ! iptables -L TOR -n >/dev/null 2>&1 ; then
+ iptables -N TOR >/dev/null 2>&1
+ iptables -A INPUT -p tcp -j TOR 2>&1
+fi
+cd /tmp/
+echo -e "\n\tGetting Tor node list from dan.me.uk\n"
+wget -q -O - https://www.dan.me.uk/torlist/ -U SXTorBlocker/1.0 > /tmp/full.tor
+iptables -F TOR
+CMD=$(cat /tmp/full.tor | uniq | sort)
+for IP in $CMD; do
+ let COUNT=COUNT+1
+ iptables -A TOR -s $IP -j DROP
+done
+iptables -A TOR -j RETURN
+echo -e "\n\tIptables now blocking TOR connections !\n"
+
