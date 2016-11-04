076918e1eff7f734a34f9e24866a615ae8fe420a
Luke <bob_monday@outlook.com>
2016-11-04 15:52:03 +0000

    Update torBlocker.sh

--- torBlocker.sh
+++ torBlocker.sh
@@ -11,8 +11,8 @@
 IPTABLES_TARGET="DROP"
 IPTABLES_CHAINNAME="TOR"
 if ! iptables -L TOR -n >/dev/null 2>&1 ; then
- iptables -N TOR >/dev/null 2>&1
- iptables -A INPUT -p tcp -j TOR 2>&1
+   iptables -N TOR >/dev/null 2>&1
+   iptables -A INPUT -p tcp -j TOR 2>&1
 fi
 cd /tmp/
 echo -e "\n\tGetting Tor node list from dan.me.uk\n"
@@ -20,8 +20,8 @@ wget -q -O - https://www.dan.me.uk/torlist/ -U SXTorBlocker/1.0 > /tmp/full.tor
 iptables -F TOR
 CMD=$(cat /tmp/full.tor | uniq | sort)
 for IP in $CMD; do
- let COUNT=COUNT+1
- iptables -A TOR -s $IP -j DROP
+    let COUNT=COUNT+1
+    iptables -A TOR -s $IP -j DROP
 done
 iptables -A TOR -j RETURN
 echo -e "\n\tIptables now blocking TOR connections !\n"
