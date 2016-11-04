0fc32bdd4ed86cb30907155b3ecd2537a9d8bf18
Luke <bob_monday@outlook.com>
2016-11-04 16:41:48 +0000

    Update apachelist.sh

--- apachelist.sh
+++ apachelist.sh
@@ -7,7 +7,7 @@
 
 set -e
 
-LOG_DIR=/var/log/apache*/*access*.log  # Any log file
+LOG_DIR=/var/log/apache*/All/access*.log  # Any log file
 
 numb=1
 
@@ -16,25 +16,30 @@ IP_WHITELIST=
 IP_LIST=$(cat ${LOG_DIR} | awk '{print $1}' | sort | uniq -c | sort -n | tail | sed 's/\s\+/ /' | cut -f 3-4 -d ' ' > iptest.txt)
 
 function check {
-
-for i in {1..10}; 
- do
-  IPLIST=$(cat iptest.txt | sed -n ${numb}p)
-  REQ_LIST=$( cat ${LOG_DIR} | awk '{print $1}' | sort | uniq -c | sort -n | tail | sed 's/\s\+/ /' | cut -f 1-2 -d ' ' | sed -n ${numb}p)
-  ADDRESS=$(echo $(whois ${IPLIST} | grep -m3 -e City: -e Country: -e country | cut -f 2-3 -d ':'))
-  ISP=$(whois ${IPLIST} | grep netname | cut -f 2 -d ':' | sed 's/\s\+/ /')
-     if grep -G $IPLIST /tmp/full.tor >> /dev/null   
-      then  
-       echo -en "${numb})" "$(tput bold)$(tput setaf 2)\tIP: $(tput sgr0)$(tput setaf 3)$IPLIST$(tput sgr0) \t"    "$(tput bold)$(tput setaf 2)REQUESTS-MADE:$(tput sgr0)$REQ_LIST \t"    "$(tput bold)$(tput setaf 4)TOR:$(tput setaf 2)TRUE$(tput sgr0) \t" "$(tput bold)$(tput setaf 2)LOCATION$(tput sgr0)|$(tput setaf 4)ISP: $(tput sgr0)$(tput setaf 2)$ADDRESS$(tput sgr0) |$(tput setaf 4)$ISP$(tput sgr0) "
-       echo
-     else
-       echo -en "${numb})" "$(tput bold)$(tput setaf 2)\tIP: $(tput sgr0)$(tput setaf 3)$IPLIST$(tput sgr0) \t"    "$(tput bold)$(tput setaf 2)REQUESTS-MADE:$(tput sgr0)$(tput setaf 3)$REQ_LIST \t"    "$(tput bold)$(tput setaf 4)TOR:$(tput setaf 1)FALSE$(tput sgr0) \t" "$(tput bold)$(tput setaf 2)LOCATION$(tput sgr0)|$(tput setaf 4)ISP: $(tput sgr0)$(tput setaf 2)$ADDRESS$(tput sgr0)|$(tput setaf 4)$ISP$(tput sgr0) "
-       echo
+    if [ -f /tmp/full.tor ]; then
+        sleep 1
+    else
+        wget -q -O - https://www.dan.me.uk/torlist/ -U SXTorBlocker/1.0 > /tmp/full.tor &&
     fi
-  let numb=numb+1
-done  
+
+    for i in {1..10};
+        do
+        IPLIST=$(cat iptest.txt | sed -n ${numb}p)
+        REQ_LIST=$( cat ${LOG_DIR} | awk '{print $1}' | sort | uniq -c | sort -n | tail | sed 's/\s\+/ /' | cut -f 1-2 -d ' ' | sed -n ${numb}p)
+        ADDRESS=$(echo $(whois ${IPLIST} | grep -m3 -e City: -e Country: -e country | cut -f 2-3 -d ':'))
+        ISP=$(whois ${IPLIST} | grep netname | cut -f 2 -d ':' | sed 's/\s\+/ /')
+        if grep -G $IPLIST /tmp/full.tor >> /dev/null
+            then
+            echo -en "${numb})" "$(tput bold)$(tput setaf 2)\tIP: $(tput sgr0)$(tput setaf 3)$IPLIST$(tput sgr0) \t"    "$(tput bold)$(tput setaf 2)REQUESTS-MADE:$(tput sgr0)$REQ_LIST \t"    "$(tput bold)$(tput setaf 4)TOR:$(tput setaf 2)TRUE$(tput sgr0) \t" "$(tput bold)$(tput setaf 2)LOCATION$(tput sgr0)|$(tput setaf 4)ISP: $(tput sgr0)$(tput setaf 2)$ADDRESS$(tput sgr0) |$(tput setaf 4)$ISP$(tput sgr0) "
+            echo
+        else
+            echo -en "${numb})" "$(tput bold)$(tput setaf 2)\tIP: $(tput sgr0)$(tput setaf 3)$IPLIST$(tput sgr0) \t"    "$(tput bold)$(tput setaf 2)REQUESTS-MADE:$(tput sgr0)$(tput setaf 3)$REQ_LIST \t"    "$(tput bold)$(tput setaf 4)TOR:$(tput setaf 1)FALSE$(tput sgr0) \t" "$(tput bold)$(tput setaf 2)LOCATION$(tput sgr0)|$(tput setaf 4)ISP: $(tput sgr0)$(tput setaf 2)$ADDRESS$(tput sgr0)|$(tput setaf 4)$ISP$(tput sgr0) "
+            echo
+        fi
+    let numb=numb+1
+    done
     echo
-    echo 
+    echo
     echo -en   "99)   $(tput setaf 1)$(tput bold) Quit"$(tput sgr0)
     echo
     echo
@@ -42,77 +47,80 @@ done
 
 
 function casein {
-echo
-echo -n "IP TRAFFIC TO LOOK INTO?:  "
-read chop
-case $chop in
-    1)
-   chop1=$(cat iptest.txt | sed -n '1p')
-   cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
     echo
-    echo
-    casein
-    ;;
-    2)
-    chop1=$(cat iptest.txt | sed -n '2p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    3)
-    chop1=$(cat iptest.txt | sed -n '3p')
-   cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    4)
-    chop1=$(cat iptest.txt | sed -n '4p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    5)
-    chop1=$(cat iptest.txt | sed -n '5p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    6)
-    chop1=$(cat iptest.txt | sed -n '6p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    7)
-    chop1=$(cat iptest.txt | sed -n '7p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    8)
-    chop1=$(cat iptest.txt | sed -n '8p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;; 
-    9)
-    chop1=$(cat iptest.txt | sed -n '9p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    10)
-    chop1=$(cat iptest.txt | sed -n '10p')
-    cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
-    casein
-    ;;
-    99)
-    echo
-    echo "Goodbye"
-    sleep 2
-    clear
-    exit 0
-    ;;
-    *)
-    echo
-    echo "1-10"
-    check_2
-    ;;
-esac
+    echo -n "IP TRAFFIC TO LOOK INTO?:  "
+    read chop
+    case $chop in
+        1)
+        chop1=$(cat iptest.txt | sed -n '1p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        echo
+        echo
+        casein
+        ;;
+        2)
+        chop1=$(cat iptest.txt | sed -n '2p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        3)
+        chop1=$(cat iptest.txt | sed -n '3p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        4)
+        chop1=$(cat iptest.txt | sed -n '4p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        5)
+        chop1=$(cat iptest.txt | sed -n '5p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        6)
+        chop1=$(cat iptest.txt | sed -n '6p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        7)
+        chop1=$(cat iptest.txt | sed -n '7p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        8)
+        chop1=$(cat iptest.txt | sed -n '8p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        9)
+        chop1=$(cat iptest.txt | sed -n '9p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        10)
+        chop1=$(cat iptest.txt | sed -n '10p')
+        cat ${LOG_DIR} | fgrep ${chop1} | awk '{print $4,$6,$7,$8,$9}' | less -X
+        casein
+        ;;
+        99)
+        echo
+        echo "Goodbye"
+        sleep 2
+        clear
+        exit 0
+        ;;
+        *)
+        echo
+        echo "1-10"
+        casein
+        ;;
+    esac
 }
 check &&
 casein
 rm iptest.txt
+if [[ ! -s /tmp/full.tor  ]]; then 
+    rm /tmp/full.tor
+fi
 exit
